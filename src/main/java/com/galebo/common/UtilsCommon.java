0cd49670bc12333ccd3ad2f700deebf2d765998b
galebo <galebo@163.com>
2014-03-12 16:54:59 +0800

    add close stream

--- src/main/java/com/galebo/common/UtilsCommon.java
+++ src/main/java/com/galebo/common/UtilsCommon.java
@@ -2,13 +2,15 @@ package com.galebo.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 
 import org.apache.commons.io.FileUtils;
 
 public class UtilsCommon {
 
-	   static public SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	   static public SimpleDateFormat sdf_yyyy_mm_dd_HH_mm_ss=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	   static public SimpleDateFormat sdf_yyyyMMddHHmm=new SimpleDateFormat("yyyyMMddHHmm");
 	   static public SimpleDateFormat sdf_yyyyMMddhhmmssSSS=new SimpleDateFormat("yyyyMMddhhmmssSSS");
 	   static public void mkdir(String fileFullName) throws IOException
@@ -16,4 +18,18 @@ public class UtilsCommon {
 			String dir = fileFullName.substring(0,fileFullName.lastIndexOf("/"));
 			FileUtils.forceMkdir(new File(dir));
 	   }
+	   static public void close(InputStream bis) {
+			if (bis != null)
+				try {
+					bis.close();
+				} catch (IOException e) {
+				}
+		}
+	   static public void close(OutputStream bis) {
+			if (bis != null)
+				try {
+					bis.close();
+				} catch (IOException e) {
+				}
+		}
 }
